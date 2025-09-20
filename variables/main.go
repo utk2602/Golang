e55e8bf287ecaad434a17106a69d768bb961f7a2
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-09-20 10:28:14 +0530

    variables

---
+++ variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integer int = 10
+	fmt.Println(integer)
+	var float float32 = 10.5
+	fmt.Println(float)
+	var boolean bool = true
+	fmt.Println(boolean)
+}
